92d992fd570201c916050c3f705fdb999f832087
nitayneeman <nitayneeman@users.noreply.github.com>
2019-02-24 11:38:03 +0200

    <RichTextInputArea/> - reorganized toolbar component

--- src/RichTextInputArea/RichTextToolbar.js
+++ src/RichTextInputArea/RichTextToolbar.js
@@ -2,21 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { RichUtils } from '@wix/draft-js';
 
+import styles from './RichTextToolbar.scss';
+import TextAreaBold from '../new-icons/system/TextAreaBold';
+import TextAreaItalic from '../new-icons/system/TextAreaItalic';
+import TextAreaUnderline from '../new-icons/system/TextAreaUnderline';
+
 const RichTextToolbar = ({ editorState, onBold, onItalic, onUnderline }) => {
-  const toggleStyle = (event, styleCallback, styleName) => {
+  const buttons = [
+    {
+      toggledStyle: 'BOLD',
+      onClick: onBold,
+      icon: TextAreaBold,
+    },
+    {
+      toggledStyle: 'ITALIC',
+      onClick: onItalic,
+      icon: TextAreaItalic,
+    },
+  ];
+
+  const toggleStyle = (event, onClick, toggledStyle) => {
     event.preventDefault();
-    styleCallback(RichUtils.toggleInlineStyle(editorState, styleName));
+    onClick(RichUtils.toggleInlineStyle(editorState, toggledStyle));
   };
 
   return (
     <div>
-      <button onClick={event => toggleStyle(event, onBold, 'BOLD')}>B</button>
-      <button onClick={event => toggleStyle(event, onItalic, 'ITALIC')}>
-        I
-      </button>
-      <button onClick={event => toggleStyle(event, onUnderline, 'UNDERLINE')}>
-        U
-      </button>
+      {buttons.map(button => {
+        const { onClick, toggledStyle, icon: Icon } = button;
+
+        return (
+          <button
+            className={styles.button}
+            onClick={event => toggleStyle(event, onClick, toggledStyle)}
+          >
+            <Icon />
+          </button>
+        );
+      })}
     </div>
   );
 };
